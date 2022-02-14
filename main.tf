6cb729fc06695ad1cf6eb5a585c9d008c7de9b4f
Alex Scarborough <alex@teak.io>
2022-02-13 20:33:50 -0800

    Remove region constraints from IAM policies
    This should simplify deploying the AMI Cleanomat across multiple
    regions, with something like
    
    provider "aws" {
    	region = "us-east-1"
    }
    
    provider "aws" {
    	alias  = "west"
    	region = "us-west-2"
    }
    
    module "cleanomat_east" {
    	source = "GoCarrot/ami-cleanomat/aws"
    }
    
    module "cleanomat_west" {
    	source = "GoCarrot/ami-cleanomat/aws"
    	providers = {
    		aws = aws.west
    	}
    
    	lambda_iam_role_arn = module.cleanomat_east.role_arn
    }

--- main.tf
+++ main.tf
@@ -27,13 +27,12 @@ terraform {
 }
 
 data "aws_partition" "current" {}
-data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 data "aws_default_tags" "tags" {}
 
 locals {
   lambda_name  = var.lambda_name
-  logs_arn     = "arn:${data.aws_partition.current.partition}:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:log-group:/aws/lambda/${local.lambda_name}"
+  logs_arn     = "arn:${data.aws_partition.current.partition}:logs:*:${data.aws_caller_identity.current.id}:log-group:/aws/lambda/${local.lambda_name}"
   iam_role_arn = coalesce(var.lambda_iam_role_arn, try(aws_iam_role.cleanomat[0].arn))
   our_tags     = var.tags
   tags         = { for key, value in local.our_tags : key => value if lookup(data.aws_default_tags.tags.tags, key, null) != value }
@@ -78,7 +77,7 @@ data "aws_iam_policy_document" "cleanomat" {
     ]
 
     resources = [
-      "arn:${data.aws_partition.current.partition}:ec2:${data.aws_region.current.name}::image/*"
+      "arn:${data.aws_partition.current.partition}:ec2:*::image/*"
     ]
 
     # TODO: Condition on tags keys to only delete AMIs created by our build process.
@@ -90,7 +89,7 @@ data "aws_iam_policy_document" "cleanomat" {
     ]
 
     resources = [
-      "arn:${data.aws_partition.current.partition}:ec2:${data.aws_region.current.name}::snapshot/*"
+      "arn:${data.aws_partition.current.partition}:ec2:*::snapshot/*"
     ]
 
     # TODO: Condition on tag keys to only delete snapshots created by our build process.
